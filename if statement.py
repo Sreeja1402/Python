2fdbcad4e6c52286d95f5f494318f5d7716dd5cb
SAI SREEJA <burrasaisreeja@gmail.com>
2024-10-10 13:04:47 +0530

    Add files via upload

---
+++ if statement.py
@@ -0,0 +1,39 @@
+##number=int(input("Enter the number: "))
+##if number%2 ==0:
+##    print("number is even")
+
+
+
+
+##a=int(input("enter a value: "))
+##b=int(input("enter a value: "))
+##c=int(input("enter a value: "))
+##if a>b and a>c:
+##    print("a is greater")
+##if b>a and b>c:
+##    print("b is greater")
+##if c>a and c>b:
+##    print("c is greater")
+
+
+##a=int(input("Enter the age: "))
+##if a>18:
+##    print("Your ar Eligible")
+##else:
+##    print("Your are not  Eligible")
+
+
+number=int(input("Enter the value: "))
+if number==10:
+    print("value is 10")
+elif number==20:
+    print("value is 20")
+elif number==30:
+    print("value is 30")
+else:
+    print("value is not 10,20,30")
+
+
+
+
+
