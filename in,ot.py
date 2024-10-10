c8d6acf8947a4bed475217d17879931bdb12875c
SAI SREEJA <burrasaisreeja@gmail.com>
2024-10-10 12:00:30 +0530

    Add files via upload

---
+++ in,ot.py
@@ -0,0 +1,26 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:/Users/saisr/OneDrive/Desktop/Python Practice/input,output.py =======
+enter the number: 4
+4
+>>> 
+======= RESTART: C:/Users/saisr/OneDrive/Desktop/Python Practice/input,output.py =======
+enter the number4546455
+Traceback (most recent call last):
+  File "C:/Users/saisr/OneDrive/Desktop/Python Practice/input,output.py", line 6, in <module>
+    d=x+y
+TypeError: can only concatenate str (not "int") to str
+>>> 
+======= RESTART: C:/Users/saisr/OneDrive/Desktop/Python Practice/input,output.py =======
+enter the number: 3
+5
+>>> 
+======= RESTART: C:/Users/saisr/OneDrive/Desktop/Python Practice/input,output.py =======
+Enter the number: 3
+Enter the number: 5
+35
+<class 'str'>
+>>> 
+======= RESTART: C:/Users/saisr/OneDrive/Desktop/Python Practice/input,output.py =======
+abcdef
---
+++ input,output.py
@@ -0,0 +1,23 @@
+x=input("enter the number: ")
+print(x)
+
+x=int(input("enter the number: "))
+y=2
+d= x+y
+print(d)
+
+
+
+a=input("Enter the number: ")
+b=input("Enter the number: ")
+
+c=a+b
+print(c)
+print(type(c))
+
+
+a='abc'
+b='def'
+print(a+b)
+
+
